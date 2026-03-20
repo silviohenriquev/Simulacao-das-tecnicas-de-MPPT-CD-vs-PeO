0c85c0e2778bf24d09b42e8cb5bf92f4a7a6b421
silviohenriquev <shvlpinto@gmail.com>
2026-03-20 15:12:32 -0300

    Finalizado

--- src/modules.py
+++ src/modules.py
@@ -1,19 +1,3 @@
-
-# modules = {
-#     'Canadian Solar CS7N-700TB-AG': {
-#         'Name': 'Canadian Solar CS7N-700TB-AG', # Nome/modelo do módulo fotovoltaico
-#         'N_s': 66, #  Número de células fotovoltaicas conectadas em série no módulo
-#         'I_sc_ref': 18.49, # Corrente de curto-circuito (Isc) em condições padrão STC (A)
-#         'V_oc_ref': 47.9, # Tensão de circuito aberto (Voc) em STC (V)
-#         'I_mp_ref': 17.51, # Corrente no ponto de máxima potência (Imp) em STC (A)
-#         'V_mp_ref': 40, # Tensão no ponto de máxima potência (Vmp) em STC (V)
-#         'T_coef_sc': 0.05, # Coeficiente de temperatura da corrente de curto-circuito (%/°C)
-#         'T_coef_oc': -0.25, # Coeficiente de temperatura da tensão de circuito aberto (%/°C)
-#         'T_coef_pmp': -0.29, # Coeficiente de temperatura da tensão de circuito aberto (%/°C)
-#     }
-# }
-
-
 modules = {
     'Canadian Solar CS7N-700TB-AG': {
         'Name': 'Canadian Solar CS7N-700TB-AG', # Nome/modelo do módulo fotovoltaico
